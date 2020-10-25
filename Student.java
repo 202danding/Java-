4085f9470261f93e4304e35b78b68139858a23ac
202danding <65993224+202danding@users.noreply.github.com>
2020-10-25 10:44:03 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,19 +1,18 @@
 package a;
-
+//子类
 public class Student extends Person{
 	String major;
-	Course course;
-	public Student(){
-		super();
-		
-	}
-	public Student(int id,String name,boolean ismale){
-		super();
-		this.id=id;
-		this.name=name;
-		this.major=major;
-		
-	}
+	//Course course;
 	
+		
+	}
+	public Student(int id,String name,char sex，String major){
+		super(id,name,sex);
+		this.major=major;
+	public String toString(){
+		return '学生信息：'姓名'+name+'学号'+id+'性别'+sex;
+	}
+
+
 
 }
